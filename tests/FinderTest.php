d829db24a8f672545c31349842d676c5ffea32e8
--unset-all <--unset-all>
2014-08-02 07:18:09 +0700

    Clean up FinderTest

--- tests/FinderTest.php
+++ tests/FinderTest.php
@@ -5,27 +5,34 @@ use Pingpong\Modules\Finder;
 
 class FinderTest extends PHPUnit_Framework_TestCase
 {
+    protected $config;
+    protected $files;
+    protected $finder;
+
     function tearDown()
     {
         m::close();
     }
 
+    function setUp()
+    {
+        $this->config = m::mock('Illuminate\Config\Repository');
+        $this->files  = m::mock('Illuminate\Filesystem\Filesystem');
+
+        $this->finder = new Finder($this->files, $this->config);
+        $this->finder->setPath($this->getPath());
+    }
+
     protected function getPath()
     {
-        return __DIR__ . '/../public/';
+        return realpath(__DIR__ . '/../public');
     }
 
     function testGetAllModules()
     {
-        $config = m::mock('Illuminate\Config\Repository');
-        $files  = m::mock('Illuminate\Filesystem\Filesystem');
+        $this->files->shouldReceive('directories')->once()->andReturn(array('Blog'));
 
-        $finder = new Finder($files, $config);
-        $finder->setPath($this->getPath());
-
-        $files->shouldReceive('directories')->once()->andReturn(array('Blog'));
-
-        $all = $finder->all();
+        $all = $this->finder->all();
 
         $this->assertTrue(is_array($all));
         $this->assertArrayHasKey(0, $all);
@@ -34,43 +41,27 @@ class FinderTest extends PHPUnit_Framework_TestCase
 
     function testHasAModule()
     {
-        $config = m::mock('Illuminate\Config\Repository');
-        $files  = m::mock('Illuminate\Filesystem\Filesystem');
+        $this->files->shouldReceive('directories')->times(2)->andReturn(array('default'));
 
-        $finder = new Finder($files, $config);
-        $finder->setPath($this->getPath());
-
-        $files->shouldReceive('directories')->times(2)->andReturn(['default']);
-
-        $this->assertTrue($finder->has('default'));
-        $this->assertNotTrue($finder->has('white'));
+        $this->assertTrue($this->finder->has('default'));
+        $this->assertNotTrue($this->finder->has('white'));
     }
 
     function testSetModulePath()
     {
-        $config = m::mock('Illuminate\Config\Repository');
-        $files  = m::mock('Illuminate\Filesystem\Filesystem');
-
-        $finder = new Finder($files, $config);
-
-        $theFinder = $finder->setPath($this->getPath());
+        $theFinder = $this->finder->setPath($this->getPath());
 
         $this->assertInstanceOf('Pingpong\Modules\Finder', $theFinder);
-        $this->assertInstanceOf('Pingpong\Modules\Finder', $finder);
+        $this->assertEquals($this->getPath(), $theFinder->getPath());
     }
 
-    function testGetModulePath()
+    function testGetModulePath ()
     {
-        $config = m::mock('Illuminate\Config\Repository');
-        $files  = m::mock('Illuminate\Filesystem\Filesystem');
+        $this->config->shouldReceive('get')->times(3)->andReturn($this->getPath());
+        $this->files->shouldReceive('directories')->times(2)->andReturn(array('default'));
 
-        $finder = new Finder($files, $config);
-
-        $config->shouldReceive('get')->times(3)->andReturn($this->getPath());
-        $files->shouldReceive('directories')->times(2)->andReturn(['default']);
-
-        $modulePath1 = $finder->getModulePath('default');
-        $modulePath2 = $finder->getModulePath('white');
+        $modulePath1 = $this->finder->getModulePath('default');
+        $modulePath2 = $this->finder->getModulePath('white');
 
         $this->assertEquals($modulePath1, $this->getPath() . "/default");
         $this->assertNull($modulePath2);
