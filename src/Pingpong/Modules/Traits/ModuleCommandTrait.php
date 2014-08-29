751e051ff5a212012fd40092d0aa4d42069c916a
gravitano <gue.warsono@gmail.com>
2014-08-30 01:51:58 +0700

    Added some doc block

--- src/Pingpong/Modules/Traits/ModuleCommandTrait.php
+++ src/Pingpong/Modules/Traits/ModuleCommandTrait.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Str;
 
 trait ModuleCommandTrait {
 	
+	/**
+	 * Get the module name.
+	 * 
+	 * @return string
+	 */
 	public function getModuleName()
 	{
 		$module = $this->argument('module') ?: $this->laravel['modules']->getUsedNow();
