5c88f4433414412ab0a97658e2496040e7a555e3
gravitano <gue.warsono@gmail.com>
2014-09-13 20:11:41 +0700

    Added generators

---
+++ src/Pingpong/Modules/Contacts/GeneratorInterface.php
@@ -0,0 +1,7 @@
+<?php namespace Pingpong\Modules\Contacts;
+
+interface GeneratorInterface {
+
+	public function generate();
+	
+}
---
+++ src/Pingpong/Modules/Generators/Generator.php
@@ -0,0 +1,7 @@
+<?php namespace Pingpong\Modules\Generators;
+
+abstract class Generator implements GeneratorInterface {
+
+	public function generate() {}
+
+}
