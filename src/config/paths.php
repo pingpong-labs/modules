3e803c8a102b986775da981859a4c404904a863e
gravitano <gue.warsono@gmail.com>
2014-09-22 23:34:51 +0700

    Added comment

--- src/config/paths.php
+++ src/config/paths.php
@@ -1,11 +1,35 @@
 <?php
 
 return [
-
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Modules path
+    |--------------------------------------------------------------------------
+    |
+    | Here you may update the modules path.
+    |
+    */
     'modules'	=>	app_path('Modules'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Modules assets path
+    |--------------------------------------------------------------------------
+    |
+    | Here you may update the modules assets path.
+    |
+    */
 	'assets'	=>	public_path('modules'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Generator path
+    |--------------------------------------------------------------------------
+    | 
+    | Here you may update the modules generator path.
+    |
+    */
     'generator' => [
         'controller' => 'Http/Controllers',
         'filter' => 'Http/Filters',
