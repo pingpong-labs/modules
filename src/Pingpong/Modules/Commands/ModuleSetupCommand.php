32d8a92fae521e40ca0f27f53b2ec9729a9823d3
gravitano <gue.warsono@gmail.com>
2014-09-25 22:02:34 +0700

    Improvement in module:setup command

--- src/Pingpong/Modules/Commands/ModuleSetupCommand.php
+++ src/Pingpong/Modules/Commands/ModuleSetupCommand.php
@@ -1,7 +1,6 @@
 <?php namespace Pingpong\Modules\Commands;
 
 use Illuminate\Console\Command;
-use Pingpong\Modules\Handlers\ModuleSetupHandler;
 
 class ModuleSetupCommand extends Command {
 
@@ -20,27 +19,57 @@ class ModuleSetupCommand extends Command {
     protected $description = 'Setting up modules folders for first use.';
 
     /**
-     * @var ModuleSetupHandler
-     */
-    protected $handler;
-
-    /**
-     * @param ModuleSetupHandler $handler
-     */
-    public function __construct(ModuleSetupHandler $handler)
-    {
-        parent::__construct();
-
-        $this->handler = $handler;
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function fire()
     {
-        return $this->handler->fire($this);
+        $this->generateModulesFolder();
+
+        $this->generateAssetsFolder();
+    }
+
+    /**
+     * Generate the modules folder.
+     */
+    public function generateModulesFolder()
+    {
+        $this->generateDirectory($this->laravel['config']->get('modules::paths.modules'),
+            'Modules directory created successfully',
+            'Modules directory already exist'
+        );
+    }
+
+    /**
+     * Generate the assets folder.
+     */
+    public function generateAssetsFolder()
+    {
+        $this->generateDirectory($this->laravel['config']->get('modules::paths.assets'),
+            'Assets directory created successfully',
+            'Assets directory already exist'
+        );
+    }
+
+    /**
+     * Generate the specified directory by given $dir.
+     *
+     * @param $dir
+     * @param $success
+     * @param $error
+     */
+    protected function generateDirectory($dir, $success, $error)
+    {
+        if ( ! $this->laravel['files']->isDirectory($dir))
+        {
+            $this->laravel['files']->makeDirectory($dir);
+
+            $this->info($success);
+
+            return;
+        }
+
+        $this->error($error);
     }
 }
