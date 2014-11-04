7c4b255e4c5e5e2c73e2d412b17d478a5a82ac79
gravitano <gue.warsono@gmail.com>
2014-11-04 19:45:00 +0700

    Added Module class

--- src/Pingpong/Modules/Module.php
+++ src/Pingpong/Modules/Module.php
@@ -2,4 +2,66 @@
 
 class Module {
 
+	protected $name;
+
+	protected $repository;
+
+	public function __construct($name, $repository = null)
+	{
+		$this->name = $name;
+		$this->repository = $repository;
+	}
+
+	public function setRepository(Repository $repository)
+	{
+		$this->repository = $repository;
+
+		return $this;
+	}
+
+	public function getRepository()
+	{
+		return $this->repository;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getLowerName()
+	{
+		return strtolower($name);
+	}
+
+	public function getPath()
+	{
+		return $this->repository->getModulePath($this->name);
+	}
+
+	public function active()
+	{
+		return $this->repository->active($this->name);
+	}
+
+	public function enable()
+	{
+		return $this->repository->enable($this->name);
+	}
+
+	public function disable()
+	{
+		return $this->repository->disable($this->name);
+	}
+
+	public function notActive()
+	{
+		return ! $this->active();
+	}
+
+	public function __toString()
+	{
+		return $this->name;
+	}
+
 }
