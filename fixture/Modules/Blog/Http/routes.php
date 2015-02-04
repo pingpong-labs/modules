2d11b9b9f51762fec155071717382cba7e0d533b
gravitano <gravitano16@gmail.com>
2015-02-04 11:43:45 +0700

    Sync & update file structure for pingpong/modules

--- fixture/Modules/Blog/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'blog', 'namespace' => 'Modules\Blog\Http\Controllers'], function()
-{
-	Route::get('/', 'BlogController@index');
-});
--- fixture/Modules/Blog/Resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('blog::layouts.master')
-
-@section('content')
-	
-	<h1>Hello World</h1>
-	
-@stop
