24ae569778fff725393a56f2b984225819eb9c1a
Gravitano <gravitano16@gmail.com>
2015-04-20 00:24:07 +0700

    update stub path

--- Commands/ModelCommand.php
+++ Commands/ModelCommand.php
@@ -54,12 +54,12 @@ class ModelCommand extends GeneratorCommand {
      */
     protected function getTemplateContents()
     {
-        return new Stub('model', [
+        return (new Stub('/model.stub', [
             'MODULE' => $this->getModuleName(),
             'NAME' => $this->getModelName(),
             'FILLABLE' => $this->getFillable(),
             'MODULE_NAMESPACE' => $this->laravel['modules']->config('namespace')
-        ]);
+        ]))->render();
     }
 
     /**
