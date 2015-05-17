ab9fc26108cdd6d71535c463b273bfaa74f92c8c
Gravitano <gravitano16@gmail.com>
2015-05-17 22:40:04 +0700

    Added new 'module:make-request' console command. Fix pingpong-labs/modules#155

---
+++ Commands/MakeRequestCommand.php
@@ -0,0 +1,71 @@
+<?php namespace Pingpong\Modules\Commands;
+
+use Illuminate\Support\Str;
+use Pingpong\Generators\Stub;
+use Pingpong\Modules\Traits\ModuleCommandTrait;
+use Symfony\Component\Console\Input\InputArgument;
+
+class MakeRequestCommand extends GeneratorCommand {
+
+    use ModuleCommandTrait;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'module:make-request';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate new form request class for the specified module.';
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            array('name', InputArgument::REQUIRED, 'The name of the form request class.'),
+            array('module', InputArgument::OPTIONAL, 'The name of module will be used.'),
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getTemplateContents()
+    {
+        return (new Stub('/request.stub', [
+            'MODULE' => $this->getModuleName(),
+            'NAME' => $this->getFileName(),
+            'MODULE_NAMESPACE' => $this->laravel['modules']->config('namespace')
+        ]))->render();
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getDestinationFilePath()
+    {
+        $path = $this->laravel['modules']->getModulePath($this->getModuleName());
+
+        $seederPath = $this->laravel['modules']->config('paths.generator.request');
+
+        return $path . $seederPath . '/' . $this->getFileName() . '.php';
+    }
+
+    /**
+     * @return string
+     */
+    private function getFileName()
+    {
+        return Str::studly($this->argument('name'));
+    }
+
+}
--- Providers/ConsoleServiceProvider.php
+++ Providers/ConsoleServiceProvider.php
@@ -48,6 +48,7 @@ class ConsoleServiceProvider extends ServiceProvider {
         'Update',
         'Use',
         'Dump',
+        'MakeRequest',
     ];
 
     /**
