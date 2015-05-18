0850c75f54a1877c82246b3fd43713fdadbf2526
Gravitano <gravitano16@gmail.com>
2015-05-19 00:01:29 +0700

    improved module:make-middleware command

--- Commands/GenerateFilterCommand.php
+++ Commands/GenerateFilterCommand.php
@@ -7,8 +7,14 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class GenerateFilterCommand extends GeneratorCommand
 {
-
     use ModuleCommandTrait;
+
+    /**
+     * The name of argument name.
+     * 
+     * @var string
+     */
+    protected $argumentName = 'name';
 
     /**
      * The console command name.
@@ -22,7 +28,7 @@ class GenerateFilterCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $description = 'Generate new filter for the specified module.';
+    protected $description = 'Generate new middleware class for the specified module.';
 
     /**
      * Get the console command arguments.
@@ -42,10 +48,11 @@ class GenerateFilterCommand extends GeneratorCommand
      */
     protected function getTemplateContents()
     {
+        $module = $this->laravel['modules']->findOrFail($this->getModuleName());
+        
         return (new Stub('/filter.stub', [
-            'MODULE' => $this->getModuleName(),
-            'NAME' => $this->getFileName(),
-            'MODULE_NAMESPACE' => $this->laravel['modules']->config('namespace')
+            'NAMESPACE' => $this->getClassNamespace($module),
+            'CLASS' => $this->getClass()
         ]))->render();
     }
 
@@ -67,5 +74,15 @@ class GenerateFilterCommand extends GeneratorCommand
     private function getFileName()
     {
         return Str::studly($this->argument('name'));
+    }
+
+    /**
+     * Get default namespace.
+     * 
+     * @return string
+     */
+    public function getDefaultNamespace()
+    {
+        return 'Http\Middleware';
     }
 }
